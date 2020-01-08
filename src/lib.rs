d84d9823fb7ab2848d4a46a6c8cb058575afac8b
William Laeder <codylaeder@gmail.com>
2020-01-08 14:24:00 -0800

    Bug patches and Preformance Fixes
    - Added many typed getter methods wich should help with access patterns.
    - Moved array parsing to occur _after_ socket parsing so you can actually get sockets

--- src/lib.rs
+++ src/lib.rs
@@ -11,9 +11,9 @@
 //! This crate will preform some basic serialization of Envir
 //! Var key-values in the following manner:
 //!
-//! * `0x[a-fA-F0-9]`: Will be converted to `i64`.
-//! * `0o[0-7]+`: Will be converted to `i64`.
-//! * `[+|-]?[0-9]`: Will be converted to `i64`.
+//! * `0x[a-fA-F0-9]`: Will be converted to `i64`, and be assumed to be hexidecimal.
+//! * `0o[0-7]+`: Will be converted to `i64`, and be assumed to be octal.
+//! * `[+|-]?[0-9]`: Will be converted to `i64`, and assumed to be decimal.
 //! * Most common patterns of floating point (`0.5`, `.5`, `3.14`, `-3.14`, `2.5E10`, etc.) will be converted to `f64`
 //! * IP addresses will be converted to `IpAddr`.
 //! * SocketAddresses (i.e.: `127.0.0.1:666`) will be converted into `SocketAddr`.
@@ -21,6 +21,8 @@
 //! * `true`, `false`, `T`, `F`, `TRUE`, `FALSE` will become `bool`.
 //!
 //! Orginal values are faithfully perserved so if the orginal string is wanted it will always be recoverable.
+
+#![allow(clippy::needless_lifetimes, clippy::match_ref_pats)]
 
 extern crate regex;
 #[macro_use]
@@ -36,7 +38,7 @@ mod socketaddr;
 use self::socketaddr::{parse_ip, parse_socket};
 
 use std::collections::HashMap;
-use std::net::{IpAddr, SocketAddr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
 use std::sync::Arc;
 
 /// Value contains the pre & post encoding information about a value.
@@ -53,6 +55,57 @@ pub enum Value {
     IpAddr(IpAddr, String),
     Array(Box<[Value]>, String),
 }
+impl Value {
+    /// as_str will always succeed as it will always fallback to the `String` stored with each value.
+    pub fn as_str<'a>(&'a self) -> Option<&'a str> {
+        match self {
+            &Value::String(ref s)
+            | &Value::Bool(_, ref s)
+            | &Value::Int(_, ref s)
+            | &Value::Float(_, ref s)
+            | &Value::SocketAddr(_, ref s)
+            | &Value::IpAddr(_, ref s)
+            | &Value::Array(_, ref s) => Some(s),
+        }
+    }
+
+    pub fn as_bool<'a>(&'a self) -> Option<&'a bool> {
+        match self {
+            &Value::Bool(ref b, _) => Some(b),
+            _ => None,
+        }
+    }
+    pub fn as_int<'a>(&'a self) -> Option<&'a i64> {
+        match self {
+            &Value::Int(ref i, _) => Some(i),
+            _ => None,
+        }
+    }
+    pub fn as_float<'a>(&'a self) -> Option<&'a f64> {
+        match self {
+            &Value::Float(ref f, _) => Some(f),
+            _ => None,
+        }
+    }
+    pub fn as_socket<'a>(&'a self) -> Option<&'a SocketAddr> {
+        match self {
+            &Value::SocketAddr(ref s, _) => Some(s),
+            _ => None,
+        }
+    }
+    pub fn as_ipv4<'a>(&'a self) -> Option<&'a Ipv4Addr> {
+        match self {
+            &Value::IpAddr(IpAddr::V4(ref i), _) => Some(i),
+            _ => None,
+        }
+    }
+    pub fn as_ipv6<'a>(&'a self) -> Option<&'a Ipv6Addr> {
+        match self {
+            &Value::IpAddr(IpAddr::V6(ref i), _) => Some(i),
+            _ => None,
+        }
+    }
+}
 
 /// Envir is a representation of the environment. But a handful of
 /// conventions and opinions are applied to the data which is found
@@ -62,7 +115,8 @@ pub enum Value {
 /// start up. This is because modifying your environment key-values post
 /// startup is generally considered an anti-pattern.
 ///
-/// Envir is sync/send safe as internally it uses `Arc<T>`.
+/// Envir is sync/send safe as internally it uses `Arc<T>`. None of its
+/// methods allow for mutation of the interior data.
 #[derive(Clone)]
 pub struct Envir {
     data: Arc<HashMap<String, Value>>,
@@ -85,6 +139,42 @@ impl Envir {
     pub fn get<'a, S: AsRef<str>>(&'a self, k: &S) -> Option<&'a Value> {
         self.data.as_ref().get(k.as_ref())
     }
+    pub fn get_bool<'a, S: AsRef<str>>(&'a self, k: &S) -> Option<&'a bool> {
+        self.get(k)
+            .into_iter()
+            .flat_map(|value| value.as_bool())
+            .next()
+    }
+    pub fn get_int<'a, S: AsRef<str>>(&'a self, k: &S) -> Option<&'a i64> {
+        self.get(k)
+            .into_iter()
+            .flat_map(|value| value.as_int())
+            .next()
+    }
+    pub fn get_float<'a, S: AsRef<str>>(&'a self, k: &S) -> Option<&'a f64> {
+        self.get(k)
+            .into_iter()
+            .flat_map(|value| value.as_float())
+            .next()
+    }
+    pub fn get_ipv4<'a, S: AsRef<str>>(&'a self, k: &S) -> Option<&'a Ipv4Addr> {
+        self.get(k)
+            .into_iter()
+            .flat_map(|value| value.as_ipv4())
+            .next()
+    }
+    pub fn get_ipv6<'a, S: AsRef<str>>(&'a self, k: &S) -> Option<&'a Ipv6Addr> {
+        self.get(k)
+            .into_iter()
+            .flat_map(|value| value.as_ipv6())
+            .next()
+    }
+    pub fn get_str<'a, S: AsRef<str>>(&'a self, k: &S) -> Option<&'a str> {
+        self.get(k)
+            .into_iter()
+            .flat_map(|value| value.as_str())
+            .next()
+    }
 }
 
 impl Value {
@@ -94,12 +184,12 @@ impl Value {
     fn new(arg: String) -> Self {
         Option::None
             .into_iter()
-            .chain(Value::split(&arg))
             .chain(parse_int(&arg).map(|val| Self::Int(val, arg.clone())))
             .chain(parse_float(&arg).map(|val| Self::Float(val, arg.clone())))
             .chain(parse_bool(&arg).map(|val| Self::Bool(val, arg.clone())))
             .chain(parse_socket(&arg).map(|val| Self::SocketAddr(val, arg.clone())))
             .chain(parse_ip(&arg).map(|val| Self::IpAddr(val, arg.clone())))
+            .chain(Value::split(&arg))
             .next()
             .unwrap_or(Self::String(arg))
     }
